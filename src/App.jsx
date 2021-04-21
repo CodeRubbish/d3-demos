67e8f51fbc218628ae61d7cea9ba5386a2a4a0e4
刘星宇 <scliuxingyu@icloud.com>
2021-04-22 01:39:48 +0800

    网格布局

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React, {PureComponent} from "react";
-import {Button} from "antd";
+import css from './App.module.scss';
+import * as d3 from 'd3'
 
+const arr = d3.schemeCategory10;
 export default class App extends PureComponent {
     render() {
-        return <Button type={'primary'}>点击我</Button>
+        return <div className={css.grid}>
+            {arr.map((d, i) => <div className={css.block} style={{background: d}} key={i}>{i + 1}</div>)}
+        </div>
     }
+
 }
