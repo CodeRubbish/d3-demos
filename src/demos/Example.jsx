a6b52cbe448c08f9043daa785f400179dc0f6687
刘星宇 <scliuxingyu@icloud.com>
2021-04-22 13:23:11 +0800

    修改样例

--- src/demos/Example.jsx
+++ src/demos/Example.jsx
@@ -2,6 +2,8 @@ import React, {PureComponent} from "react";
 import * as d3 from 'd3';
 import css from '../App.module.scss';
 
+const w = 600;
+const h = 300;
 export default class Example extends PureComponent {
     constructor(props) {
         super(props);
@@ -9,7 +11,15 @@ export default class Example extends PureComponent {
     }
 
     componentDidMount() {
-        this.svg = d3
+        this.svg = d3.select(this.myRef.current)
+            .append("svg")
+            .attr('width', w)
+            .attr('height', h);
+        this.initChart();
+    }
+
+    initChart = () => {
+        // 初始化图表
     }
 
     render() {
